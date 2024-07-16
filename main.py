90deb8f3fb749de18d5fde4474ac88f1ffc50416
unknown <yashashsn555@gmail.com>
2024-07-16 13:12:40 +0530

    data_ingestion_pipeline is added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.mice_protein_expression.logging import logger
+from mice_protein_expression.logging import logger
+from mice_protein_expression.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is custom log")
+
+
+STAGE_NAME = "Data Ingestion 01"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
